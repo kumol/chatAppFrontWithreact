fb5a00e4deec07a18a4bc0a07709b0dfaf333ba2
kumol <kumol0163@gmail.com>
2020-07-30 03:26:55 +0600

    Setup all

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import React from 'react';
+import {BrowserRouter as Router ,Route} from 'react-router-dom';
 
-import {BrowserRouter as Router,Route} from 'react-router-dom';
-
-
-import Join from './components/Join';
 import Chat from './components/Chat';
-const App = ()=>(
+import Join from './components/Join';
+
+const App =()=>(
     <Router>
         <Route path="/" exact component={Join}/>
-        <Route path="/chat" exact component={Chat}/>
+        <Route path="/chat" component={Chat}/>
     </Router>
 );
-
 export default App;
