cd51606aa9cd9029106023c9e1f4cc838df1d467
kumol <kumol01763@gmail.com>
2020-08-25 23:24:32 +0600

    lst

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -41,10 +41,10 @@ const Chat = ({location })=>{
     return (
         <div className="outerContainer">
             <div className="container">
-                <input type="text " value={message}
+                {/* <input type="text " value={message}
                 onChange={(event)=>setMessage(event.target.value)}
                 onKeyPress={(event)=>event.key === 'Enter'?sendMessage(event):null}>
-                </input>
+                </input> */}
             </div>
         </div>
     )
